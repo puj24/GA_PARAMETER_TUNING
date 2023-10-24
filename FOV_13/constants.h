60cd8f2d3f70eb691979f78b21a012716bc400c0
puj24 <pujavedantu24@gmail.com>
2023-10-25 01:09:47 +0530

    Add FOV_13 images
    for GA testing by Avanti on huge ranges. Thanks to log scale

---
+++ FOV_13/constants.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1820
+#define BREADTH 1820
+#define PIXEL_WIDTH 1.55e-6
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.2e-15
+#define DELTA 1e-3
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.5
+#define P1 35
+#define P2 80
+#define EPSILON_SEQ_ERROR 0.005
+#define EPSILON_EST 0.001 
