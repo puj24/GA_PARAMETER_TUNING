bca1c2c0778b6222c0803502abf1b36709b52987
puj24 <pujavedantu24@gmail.com>
2023-11-29 01:30:19 +0530

    Add Jalna pictures & headers
    Contains captured images, FOV_13 images, UIS header files for running GA

---
+++ FOV13/UIS_headers/UIS_24.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//N_i_24 - Number of input centroids
+#define N_i_24 1
+double arr_out_UIS_24[N_i_24][3]={{1, 0.0007930028688525, 0.0002812572404372},
+};
---
+++ FOV13/UIS_headers/main.c
@@ -0,0 +1,376 @@
+// Standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+// User defined header files
+#include "UIS_1.h"
+#include "UIS_2.h"
+#include "UIS_3.h"
+#include "UIS_4.h"
+#include "UIS_5.h"
+// #include "UIS_6.h"
+// #include "UIS_7.h"
+// #include "UIS_8.h"
+// #include "UIS_9.h"
+// #include "UIS_10.h"
+#include "UIS_11.h"
+#include "UIS_12.h"
+#include "UIS_13.h"
+#include "UIS_14.h"
+#include "UIS_15.h"
+#include "UIS_16.h"
+// #include "UIS_17.h"
+#include "UIS_18.h"
+#include "UIS_19.h"
+#include "UIS_20.h"
+#include "UIS_21.h"
+#include "UIS_22.h"
+#include "UIS_23.h"
+#include "UIS_24.h"
+#include "UIS_25.h"
+#include "UIS_26.h"
+#include "UIS_27.h"
+#include "UIS_28.h"
+#include "UIS_29.h"
+#include "UIS_30.h"
+#include "UIS_31.h"
+// #include "UIS_32.h"
+// #include "UIS_33.h"
+#include "UIS_34.h"
+// #include "UIS_35.h"
+#include "UIS_36.h"
+#include "UIS_37.h"
+// #include "UIS_38.h"
+#include "UIS_39.h"
+#include "UIS_40.h"
+#include "UIS_41.h"
+#include "UIS_42.h"
+#include "UIS_43.h"
+#include "UIS_44.h"
+#include "UIS_45.h"
+#include "UIS_46.h"
+#include "UIS_47.h"
+#include "UIS_48.h"
+#include "UIS_49.h"
+#include "UIS_50.h"
+
+#include "functions.h"
+#include "constants.h"
+
+
+int sm_K_vec_arr[N_KVEC_PAIRS][3];
+
+
+int main()
+{
+  clock_t start, end;
+  start = clock();
+
+    
+//  printf("HILS_iter_1\n");
+  HILS(arr_out_UIS_1, N_i_1);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_2\n");
+  HILS(arr_out_UIS_2, N_i_2);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_3\n");
+  HILS(arr_out_UIS_3, N_i_3);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_4\n");
+  HILS(arr_out_UIS_4, N_i_4);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_5\n");
+  HILS(arr_out_UIS_5, N_i_5);
+//   printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_6\n");
+  // HILS(arr_out_UIS_6, N_i_6);
+  // printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_7\n");
+  // HILS(arr_out_UIS_7, N_i_7);
+  // printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_8\n");
+  // HILS(arr_out_UIS_8, N_i_8);
+  // printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_9\n");
+  // HILS(arr_out_UIS_9, N_i_9);
+  // printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_10\n");
+  // HILS(arr_out_UIS_10, N_i_10);
+  // printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_11\n");
+  HILS(arr_out_UIS_11, N_i_11);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_12\n");
+  HILS(arr_out_UIS_12, N_i_12);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_13\n");
+  HILS(arr_out_UIS_13, N_i_13);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_14\n");
+  HILS(arr_out_UIS_14, N_i_14);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_15\n");
+  HILS(arr_out_UIS_15, N_i_15);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_16\n");
+  HILS(arr_out_UIS_16, N_i_16);
+//   printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_17\n");
+  // HILS(arr_out_UIS_17, N_i_17);
+  // printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_18\n");
+  HILS(arr_out_UIS_18, N_i_18);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_19\n");
+  HILS(arr_out_UIS_19, N_i_19);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_20\n");
+  HILS(arr_out_UIS_20, N_i_20);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_21\n");
+  HILS(arr_out_UIS_21, N_i_21);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_22\n");
+  HILS(arr_out_UIS_22, N_i_22);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_23\n");
+  HILS(arr_out_UIS_23, N_i_23);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_24\n");
+  HILS(arr_out_UIS_24, N_i_24);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_25\n");
+  HILS(arr_out_UIS_25, N_i_25);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_26\n");
+  HILS(arr_out_UIS_26, N_i_26);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_27\n");
+  HILS(arr_out_UIS_27, N_i_27);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_28\n");
+  HILS(arr_out_UIS_28, N_i_28);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_29\n");
+  HILS(arr_out_UIS_29, N_i_29);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_30\n");
+  HILS(arr_out_UIS_30, N_i_30);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_31\n");
+  HILS(arr_out_UIS_31, N_i_31);
+//   printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_32\n");
+  // HILS(arr_out_UIS_32, N_i_32);
+  // printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_33\n");
+  // HILS(arr_out_UIS_33, N_i_33);
+  // printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_34\n");
+  HILS(arr_out_UIS_34, N_i_34);
+//   printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_35\n");
+  // HILS(arr_out_UIS_35, N_i_35);
+  // printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_36\n");
+  HILS(arr_out_UIS_36, N_i_36);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_37\n");
+  HILS(arr_out_UIS_37, N_i_37);
+//   printf("\n\n\n");
+
+
+
+  // printf("HILS_iter_38\n");
+  // HILS(arr_out_UIS_38, N_i_38);
+  // printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_39\n");
+  HILS(arr_out_UIS_39, N_i_39);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_40\n");
+  HILS(arr_out_UIS_40, N_i_40);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_41\n");
+  HILS(arr_out_UIS_41, N_i_41);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_42\n");
+  HILS(arr_out_UIS_42, N_i_42);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_43\n");
+  HILS(arr_out_UIS_43, N_i_43);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_44\n");
+  HILS(arr_out_UIS_44, N_i_44);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_45\n");
+  HILS(arr_out_UIS_45, N_i_45);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_46\n");
+  HILS(arr_out_UIS_46, N_i_46);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_47\n");
+  HILS(arr_out_UIS_47, N_i_47);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_48\n");
+  HILS(arr_out_UIS_48, N_i_48);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_49\n");
+  HILS(arr_out_UIS_49, N_i_49);
+//   printf("\n\n\n");
+
+
+
+//  printf("HILS_iter_50\n");
+  HILS(arr_out_UIS_50, N_i_50);
+//   printf("\n\n\n");
+
+    
+  end = clock();
+  double duration = ((double)end - start)/CLOCKS_PER_SEC;
+  // printf("Time taken to execute in seconds : %f\n", duration);
+}
